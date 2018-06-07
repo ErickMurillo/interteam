27db8216002808e59c03eef11cdf09e916f44893
Erick Murillo <you@example.com>
2018-06-07 08:39:29 -0600

    galeria de img y videos

--- galerias/urls.py
+++ galerias/urls.py
@@ -5,4 +5,6 @@ from .views import *
 urlpatterns = [
 	path('imagenes/', lista_galerias_img, name='lista-galerias-img'),
 	path('imagenes/<id>/', detalle_galerias_img, name='detalle-galerias-img'),
+	path('videos/', lista_galerias_videos, name='lista-galerias-videos'),
+	path('videos/<id>/', detalle_galerias_video, name='detalle-galerias-video'),
 ]
--- galerias/views.py
+++ galerias/views.py
@@ -30,3 +30,27 @@ def detalle_galerias_img(request,id, template='detalle_galeria.html'):
 		dic_temas[tema] = count
 
 	return render(request, template, locals())
+
+def lista_galerias_videos(request,template='list_galerias.html'):
+	object_list = GaleriaVideos.objects.order_by('-id')
+	tematicas = Tematica.objects.order_by('nombre')
+	if request.GET.get('tematica'):
+		x = request.GET['tematica']
+		object_list = GaleriaVideos.objects.filter(tematica = x).order_by('-id')
+	else:
+		object_list = GaleriaVideos.objects.order_by('-id')
+
+	return render(request, template, locals())
+
+def detalle_galerias_video(request,id, template='detalle_galeria_video.html'):
+	object = get_object_or_404(GaleriaVideos, id = id)
+	ultimas_notas = Notas.objects.order_by('-fecha','-id')[:4]
+	hoy = datetime.date.today()
+	eventos = Agendas.objects.filter(inicio__gte = hoy, publico = True).order_by('inicio')[:3]
+
+	dic_temas = {}
+	for tema in Temas.objects.all():
+		count = Notas.objects.filter(temas = tema).count()
+		dic_temas[tema] = count
+
+	return render(request, template, locals())
